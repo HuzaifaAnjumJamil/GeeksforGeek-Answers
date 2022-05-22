126362bb2570c2188efb736191154361310dbcc9
Huzaifa <haj.huzaifa@gmail.com>
2022-05-22 22:33:02 +0530

    Simple division-Codechef

--- makePair.cpp
+++ makePair.cpp
@@ -1,41 +1,23 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h>
+using namespace std ;
 
-int main()
-{
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		int m, x, y;
-		cin >> m >> x >> y;
-		int m_no, lr, rr;
-		int house[101] = {0};
-		for (int i = 1; i <= m; i++)
-		{
-			cin >> m_no;
-			lr = m_no - (x * y);
-			rr = m_no + (x * y);
-			for (int i = m_no; i <= rr && i <= 100; i++)
-			{
-				house[i]++;
-			}
-			for (int i = m_no; i >= lr && i >= 1; i--)
-			{
-				house[i]++;
-			}
-		}
-		int ans = 0;
-		for (int i = 1; i < 101; i++)
-		{
-			if (house[i] == 0)
-			{
-				ans++;
-			}
-		}
-
-		cout << ans << endl;
-	}
-
-	return 0;
+int main(){
+    int t ;
+    cin >>t ;
+    while(t--){
+        int n  , x , y ;
+        cin >> n >> x >> y ;
+        int a[n] ;
+        for(int i = 0 ; i < n ; i++){
+            cin>>a[i] ;
+        }
+        int count = 0 ;
+        for(int i= 0 ; i < n ; i++){
+            if(a[i] <= x && a[i]%y == 0 ){
+              count++ ;
+            }
+        }
+        cout<<count<<endl ;
+    }
+    return 0;
 }
