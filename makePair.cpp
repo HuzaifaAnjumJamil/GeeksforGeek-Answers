8883cecbfd89215b533e8caa7221be987be54383
Huzaifa <haj.huzaifa@gmail.com>
2022-05-22 17:40:40 +0530

    Cops and the Thief Devu

--- makePair.cpp
+++ makePair.cpp
@@ -3,21 +3,39 @@ using namespace std;
 
 int main()
 {
-	int a,x;
-	cin>>a;
-	while(a--){
-		x=0;
-		string b,c;
-		cin>>b>>c;
-		for(int i=0;i<b.length();i++){
-			if(b[i] != c[i] && b[i] != '?' && c[i] != '?'){
-				x = 1;
-				break;
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int m, x, y;
+		cin >> m >> x >> y;
+		int m_no, lr, rr;
+		int house[101] = {0};
+		for (int i = 1; i <= m; i++)
+		{
+			cin >> m_no;
+			lr = m_no - (x * y);
+			rr = m_no + (x * y);
+			for (int i = m_no; i <= rr && i <= 100; i++)
+			{
+				house[i]++;
+			}
+			for (int i = m_no; i >= lr && i >= 1; i--)
+			{
+				house[i]++;
 			}
 		}
-		cout << ((x ==1) ? "No" : "Yes") << endl;
+		int ans = 0;
+		for (int i = 1; i < 101; i++)
+		{
+			if (house[i] == 0)
+			{
+				ans++;
+			}
+		}
 
+		cout << ans << endl;
 	}
-	
+
 	return 0;
 }
