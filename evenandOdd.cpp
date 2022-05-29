898124fcf813adb5f7f39653adfa545e26fcd383
Huzaifa <haj.huzaifa@gmail.com>
2022-05-30 02:23:12 +0530

    count substring-codecheff

--- evenandOdd.cpp
+++ evenandOdd.cpp
@@ -1,40 +1,21 @@
-// chef in vaccination queue
-
-// pth postion from the front of the queue
-// x minutes to vaccinate a child abd y minutes to vaccinate a elderly person
-// chef is an elderly person
-
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define mod 1000000007
 
-int main()
-{
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        ll n, p, x, y;
-        cin >> n >> p >> x >> y;
-        ll a[n];
-        for (ll i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        ll ans = 0;
-        for(ll i = 0; i < p; i++)
-        {
-            if(a[i] == 1)
-            {
-                ans += y;
-            }
-            else
-            {
-                ans += x;
-            }
-        }
-        cout << ans << endl;
-    }
-    return 0;
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   int n;
+	   cin>>n;
+	   long long c=0;
+	  char a[n];
+	  for(int i=0;i<n;i++){
+	    cin>>a[i];
+        if(a[i]=='1')
+	        c++;    
+	  }
+	  cout<<(c*(c+1))/2<<endl;
+	}
+	return 0;
 }
