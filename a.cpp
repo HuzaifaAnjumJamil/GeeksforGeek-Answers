d68b75270ebb7c00c84fa70c89651c23a2bb375e
Huzaifa <haj.huzaifa@gmail.com>
2022-05-31 19:18:16 +0530

    lazy chef-codecheff

--- a.cpp
+++ a.cpp
@@ -1,22 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define ll int 
+#define mod 1000000007
 
-int main() {
-  int t;
+
+int main(){
+  ll t;
   cin>>t;
   while(t--){
-      ll n;
-      cin>>n;
-      ll nn= n;
-      int sum= 0;
-      while(nn>0){
-          sum+= nn%10;
-          nn/=10;
-      }
-      if(n%sum) cout<<"No\n";
-      else cout<<"Yes\n";
-      
+    ll x,m,d;
+    cin>>x>>m>>d;
+    ll ans=0;
+    ll a=x * m;
+    ll b=x + d;
+    ans = min(a,b);
+    cout<<ans<<endl;
+
   }
   return 0;
 }
