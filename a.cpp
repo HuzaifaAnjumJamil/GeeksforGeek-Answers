42075edaa7c377567989517c1fe6236f1beaa3a3
Huzaifa <haj.huzaifa@gmail.com>
2022-06-11 01:40:26 +0530

    Subtraction-june long-codechef

--- a.cpp
+++ a.cpp
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+// write a function to convert int n divided by 6 to ceiling of n/6
+int ceil(int n)
+{
+	return (n + 5) / 6;
+}
+
+int main()
+{
 	int t;
-	cin>>t;
-	while(t--)
+	cin >> t;
+	while (t--)
 	{
-	   int x,y;
-	   cin>>x>>y;
-	   if(x==y){
-	       cout<<0<<endl;
-	   }
-	   else if(x>y)
-	    cout<<x-y<<endl;
-	   else{
-	    cout <<0<<endl;
-	   }
+		int n, x;
+		cin >> n >> x;
+
+		if (n <= 6)
+			cout << x << endl;
+		else
+			cout << ceil(n) * x << endl;
 	}
 	return 0;
 }
