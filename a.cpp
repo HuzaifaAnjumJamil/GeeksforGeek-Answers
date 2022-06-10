3b468ded7de6909c9efa400c361621b16ef21712
Huzaifa <haj.huzaifa@gmail.com>
2022-06-11 01:04:23 +0530

    Chair Requirement-june long term -codechef

--- a.cpp
+++ a.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll int 
-#define mod 1000000007
 
-
-int main(){
-  ll t;
-  cin>>t;
-  while(t--){
-    ll a,b,c,d;
-    cin>>a>>b>>c>>d;
-    ll ans=180;
-    if((a+c) == ans && (b+d) == ans){
-      cout<<"YES"<<endl;
-    }
-    else{
-      cout<<"NO"<<endl;
-    }
-
-  }
-  return 0;
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   int x,y;
+	   cin>>x>>y;
+	   if(x==y){
+	       cout<<0<<endl;
+	   }
+	   else if(x>y)
+	    cout<<x-y<<endl;
+	   else{
+	    cout <<0<<endl;
+	   }
+	}
+	return 0;
 }
